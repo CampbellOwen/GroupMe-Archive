f5fbfac2d1914b94b689e54c0ab43f1d4a7c5407
Chris Apparicio <chris@apparicio.me>
2016-10-07 16:00:27 +0100

    moved root route back to app.js

--- server/app.js
+++ server/app.js
@@ -13,6 +13,16 @@ app.set('view engine', 'jade');
 
 require('./routes')(app, db);
 
+// leave this last, after all other routes - makes sure hitting '/' will always work and not get hijacked by the apiRouter
+// if the front end framework has a router it might be worth hitting '*' and routing at the front end
+app.get('/', function (req, res) {
+    if (req.query.access_token == undefined) {
+        res.sendFile('public/html/login.html', {root: __dirname});
+    }
+    else {
+        res.render('index', {token: req.query.access_token});
+    }
+});
 app.listen(port, function() {
    console.log('GroupMe Archive - listening on port ' + port);
 })
